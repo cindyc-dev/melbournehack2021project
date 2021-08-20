d7f26b3c07a01820e6dab54ba21a726b2815bad4
Qwek-alt <66938425+chuahxinyu@users.noreply.github.com>
2021-08-20 17:16:30 +0800

    Create React App

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # melbournehack2021project
+
+<>
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './Card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Subscription Tracker</h1>
+      <p>Testing from my side</p>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
